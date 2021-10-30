f87c9a47484b9b93f886f2edcc664073571320d4
Emmanuel Sackey <aximilli1212@gmail.com>
2021-10-30 14:18:59 +0000

    Add book display details as a fragment

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -1,17 +1,12 @@
-import {bookables} from "../../static.json";
-import {useState, Fragment} from "react";
-import {prop} from 'ramda'
+import {useState, Fragment} from 'react';
+import {bookables, sessions, days} from "../../static.json";
 import {FaArrowRight} from "react-icons/fa";
 
 export default function BookablesList () {
-
-const  [group, setGroup] = useState("Rooms")
-
+  const [group, setGroup] = useState("Kit");
   const bookablesInGroup = bookables.filter(b => b.group === group);
-
-  const [bookableIndex, setBookableIndex] = useState(1)
+  const [bookableIndex, setBookableIndex] = useState(0);
   const groups = [...new Set(bookables.map(b => b.group))];
-
   const bookable = bookablesInGroup[bookableIndex];
   const [hasDetails, setHasDetails] = useState(false);
 
@@ -19,42 +14,85 @@ const  [group, setGroup] = useState("Rooms")
     setBookableIndex(i => (i + 1) % bookablesInGroup.length);
   }
 
-  function changeBookable(selectedIndex){
-    setBookableIndex(selectedIndex)
-    console.log(selectedIndex);
-  }
-
   return (
-      <div>
-      <select
-          value={group}
-          onChange={(e) => setGroup(e.target.value)}>
-      {groups.map(g => <option value={g} key={g}>{g}</option>)}
-      </select>
-        <ul className="bookables items-list-nav">
-          {bookablesInGroup.map((b, i) => (
-              <li
-                  key={b.id}
-                  className={i === bookableIndex ? "selected" : null}
-              >
-                <button
-                    className="btn"
-                    onClick={()=>changeBookable(i)}
-                >
-                  {b.title}
-                </button>
-              </li>
-          ))}
-        </ul>
-        <p>
-          <button
-              className="btn"
-              onClick={nextBookable} autoFocus
+      <Fragment>
+        <div>
+          <select
+              value={group}
+              onChange={(e) => setGroup(e.target.value)}
           >
-            <FaArrowRight/>
-            <span>Next</span>
-          </button>
-        </p>
-      </div>
+            {groups.map(g => <option value={g} key={g}>{g}</option>)}
+          </select>
+
+          <ul className="bookables items-list-nav">
+            {bookablesInGroup.map((b, i) => (
+                <li
+                    key={b.id}
+                    className={i === bookableIndex ? "selected" : null}
+                >
+                  <button
+                      className="btn"
+                      onClick={() => setBookableIndex(i)}
+                  >
+                    {b.title}
+                  </button>
+                </li>
+            ))}
+          </ul>
+          <p>
+            <button
+                className="btn"
+                onClick={nextBookable}
+                autoFocus
+            >
+              <FaArrowRight/>
+              <span>Next</span>
+            </button>
+          </p>
+        </div>
+
+        {bookable && (
+            <div className="bookable-details">
+              <div className="item">
+                <div className="item-header">
+                  <h2>
+                    {bookable.title}
+                  </h2>
+                  <span className="controls">
+                <label>
+                  <input
+                      type="checkbox"
+                      checked={hasDetails}
+                      onChange={() => setHasDetails(has => !has)}
+                  />
+                  Show Details
+                </label>
+              </span>
+                </div>
+
+                <p>{bookable.notes}</p>
+
+                {hasDetails && (
+                    <div className="item-details">
+                      <h3>Availability</h3>
+                      <div className="bookable-availability">
+                        <ul>
+                          {bookable.days
+                              .sort()
+                              .map(d => <li key={d}>{days[d]}</li>)
+                          }
+                        </ul>
+                        <ul>
+                          {bookable.sessions
+                              .map(s => <li key={s}>{sessions[s]}</li>)
+                          }
+                        </ul>
+                      </div>
+                    </div>
+                )}
+              </div>
+            </div>
+        )}
+      </Fragment>
   );
 }
--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, Fragment} from 'react';
 import {users} from "../../static.json";
 
 export default function UsersList () {
